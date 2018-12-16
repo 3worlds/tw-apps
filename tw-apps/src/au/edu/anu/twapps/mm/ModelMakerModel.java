acb6a532c326eb737fc431045282ef12a06fb05c
Ian Davies <u9007333@anu.edu.au>
2018-12-16 17:49:29 +1100

    refactoring to twapps

--- tw-apps/src/au/edu/anu/twapps/mm/ModelMakerModel.java
+++ tw-apps/src/au/edu/anu/twapps/mm/ModelMakerModel.java
@@ -1,30 +1,31 @@
 /**************************************************************************
- *  TW-MM - ThreeWorlds ModelMaker                                        *
+ *  TW-APPS - Applications used by 3Worlds                                *
  *                                                                        *
  *  Copyright 2018: Jacques Gignoux & Ian D. Davies                       *
  *       jacques.gignoux@upmc.fr                                          *
  *       ian.davies@anu.edu.au                                            * 
  *                                                                        *
- *  TW-MM is the UI-independent code for ModelMaker                       *
+ *  TW-APPS contains ModelMaker and ModelRunner, programs used to         *
+ *  construct and run 3Worlds configuration graphs. All code herein is    *
+ *  independent of UI implementation.                                     *
  *                                                                        *
  **************************************************************************                                       
- *  This file is part of TW-MM (ThreeWorlds-ModelMakert).                 *
+ *  This file is part of TW-APPS (3Worlds applications).                  *
  *                                                                        *
- *  TW-MM is free software: you can redistribute it and/or modify         *
+ *  TW-APPS is free software: you can redistribute it and/or modify       *
  *  it under the terms of the GNU General Public License as published by  *
  *  the Free Software Foundation, either version 3 of the License, or     *
  *  (at your option) any later version.                                   *
  *                                                                        *
- *  TW-MM is distributed in the hope that it will be useful,              *
+ *  TW-APPS is distributed in the hope that it will be useful,            *
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of        *
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
  *  GNU General Public License for more details.                          *                         
  *                                                                        *
  *  You should have received a copy of the GNU General Public License     *
- *  along with TW-MM.                                                     *
- *  If not, see <https://www.gnu.org/licenses/gpl.html>.                  *
- *                                                                        *
- **************************************************************************/
+ *  along with TW-APPS.                                                   *
+ *  If not, see <https://www.gnu.org/licenses/gpl.html>                   *
+  **************************************************************************/
 
 package au.edu.anu.twapps.mm;
 
--- tw-apps/src/au/edu/anu/twapps/project/Project.java
+++ tw-apps/src/au/edu/anu/twapps/project/Project.java
@@ -1,32 +1,32 @@
 /**************************************************************************
- *  TW-CORE - 3Worlds Core classes and methods                            *
+ *  TW-APPS - Applications used by 3Worlds                                *
  *                                                                        *
- *  Copyright 2018: Shayne Flint, Jacques Gignoux & Ian D. Davies         *
- *       shayne.flint@anu.edu.au                                          * 
+ *  Copyright 2018: Jacques Gignoux & Ian D. Davies                       *
  *       jacques.gignoux@upmc.fr                                          *
  *       ian.davies@anu.edu.au                                            * 
  *                                                                        *
- *  TW-CORE is a library of the principle components required by 3W       *
+ *  TW-APPS contains ModelMaker and ModelRunner, programs used to         *
+ *  construct and run 3Worlds configuration graphs. All code herein is    *
+ *  independent of UI implementation.                                     *
  *                                                                        *
  **************************************************************************                                       
- *  This file is part of TW-CORE (3Worlds Core).                          *
+ *  This file is part of TW-APPS (3Worlds applications).                  *
  *                                                                        *
- *  TW-CORE is free software: you can redistribute it and/or modify       *
+ *  TW-APPS is free software: you can redistribute it and/or modify       *
  *  it under the terms of the GNU General Public License as published by  *
  *  the Free Software Foundation, either version 3 of the License, or     *
  *  (at your option) any later version.                                   *
  *                                                                        *
- *  TW-CORE is distributed in the hope that it will be useful,            *
+ *  TW-APPS is distributed in the hope that it will be useful,            *
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of        *
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
  *  GNU General Public License for more details.                          *                         
  *                                                                        *
  *  You should have received a copy of the GNU General Public License     *
- *  along with TW-CORE.                                                   *
+ *  along with TW-APPS.                                                   *
  *  If not, see <https://www.gnu.org/licenses/gpl.html>                   *
- *                                                                        *
- **************************************************************************/
-
+  **************************************************************************/
+ 
 package au.edu.anu.twapps.project;
 
 import java.io.File;
