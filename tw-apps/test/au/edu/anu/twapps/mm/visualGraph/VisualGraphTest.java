eb92eb66263a3b3a0f184fa834155d28fc65f1f3
daviesi <u9007333@anu.edu.au>
2019-05-01 11:59:35 +1000

    issue #2

--- tw-apps/test/au/edu/anu/twapps/mm/visualGraph/VisualGraphTest.java
+++ tw-apps/test/au/edu/anu/twapps/mm/visualGraph/VisualGraphTest.java
@@ -19,8 +19,8 @@ class VisualGraphTest {
 		AotNode croot = cg.makeTreeNode(null, Configuration.N_ROOT+PairIdentity.LABEL_NAME_STR_SEPARATOR+name);
 		VisualNode vroot = vg.makeTreeNode(null, Configuration.N_ROOT+PairIdentity.LABEL_NAME_STR_SEPARATOR+name);
 		
-		AotNode csrcNode = cg.makeTreeNode(croot,Configuration.N_CODESOURCE+PairIdentity.LABEL_NAME_STR_SEPARATOR+name);
-		VisualNode vsrcNode = vg.makeTreeNode(vroot,Configuration.N_CODESOURCE+PairIdentity.LABEL_NAME_STR_SEPARATOR+name);
+		AotNode csrcNode = cg.makeTreeNode(croot,Configuration.N_DATADEFINITION+PairIdentity.LABEL_NAME_STR_SEPARATOR+name);
+		VisualNode vsrcNode = vg.makeTreeNode(vroot,Configuration.N_DATADEFINITION+PairIdentity.LABEL_NAME_STR_SEPARATOR+name);
 		assertTrue(croot!=null);
 		assertTrue(croot.getLabel().equals(Configuration.N_ROOT));
 		vroot.setConfigNode(croot);
