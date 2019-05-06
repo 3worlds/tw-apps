484dbb23ed1e9c932cc16822763e35cfd5832ffb
Jacques Gignoux <jacques.gignoux@upmc.fr>
2019-05-06 15:02:28 +0200

    upgrade to version 0.1.0

--- tw-apps/scripts/fr/cnrs/iees/versioning/VersionSettings.java
+++ tw-apps/scripts/fr/cnrs/iees/versioning/VersionSettings.java
@@ -54,10 +54,10 @@ public class VersionSettings {
 	 * 
 	 */
 	protected static String[][] DEPS = { 
-			{"fr.ens.biologie", "generics", "[0.0.6,)", null},
-			{"au.edu.anu.rscs.aot", "omugi", "[0.0.14,)", null},
-			{"fr.cnrs.iees.tw-core", "tw-core", "[0.0.8,)", null},
-			{"au.edu.anu.rscs.aot", "aot", "[0.0.10,)", null},
+			{"fr.ens.biologie", "generics", "[0.1.0,)", null},
+			{"au.edu.anu.rscs.aot", "omugi", "[0.1.0,)", null},
+			{"fr.cnrs.iees.tw-core", "tw-core", "[0.1.0,)", null},
+			{"au.edu.anu.rscs.aot", "aot", "[0.1.0,)", null},
 			{"org.apache.commons", "commons-text", "1.+", null},
 			{"org.openjfx", "javafx-fxml", "11+", "_os"},
 			{"org.openjfx", "javafx-controls", "11+", "_os"},
