1e9d10cc074d4d589add94ea8e3ad035f3fc5387
Jacques Gignoux <jacques.gignoux@upmc.fr>
2019-09-16 10:28:12 +0200

    class loading fix

--- tw-apps/scripts/fr/cnrs/iees/versioning/VersionSettings.java
+++ tw-apps/scripts/fr/cnrs/iees/versioning/VersionSettings.java
@@ -54,11 +54,11 @@ public class VersionSettings {
 	 * 
 	 */
 	protected static String[][] DEPS = { 
-			{"fr.ens.biologie", "generics", "[0.1.10,)", null},
-			{"au.edu.anu.rscs.aot", "omugi", "[0.2.10,)", null},
-			{"au.edu.anu.rscs.aot", "qgraph", "[0.1.2,)", null},
-			{"au.edu.anu.rscs.aot", "aot", "[0.1.5,)", null},
-			{"fr.cnrs.iees.tw-core", "tw-core", "[0.2.3,)", null},
+			{"fr.ens.biologie", "generics", "[0.1.12,)", null},
+			{"au.edu.anu.rscs.aot", "omugi", "[0.2.12,)", null},
+			{"au.edu.anu.rscs.aot", "qgraph", "[0.1.3,)", null},
+			{"au.edu.anu.rscs.aot", "aot", "[0.1.6,)", null},
+			{"fr.cnrs.iees.tw-core", "tw-core", "[0.2.4,)", null},
 			{"org.apache.commons", "commons-text", "1.+", null},
 	};
 	
