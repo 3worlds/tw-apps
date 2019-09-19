0623c1fae3da7363f9340058d58d258323040a58
Jacques Gignoux <jacques.gignoux@upmc.fr>
2019-09-19 10:53:01 +0200

    dependency consistency

--- tw-apps/scripts/fr/cnrs/iees/versioning/VersionSettings.java
+++ tw-apps/scripts/fr/cnrs/iees/versioning/VersionSettings.java
@@ -55,10 +55,10 @@ public class VersionSettings {
 	 */
 	protected static String[][] DEPS = { 
 			{"fr.ens.biologie", "generics", "[0.1.12,)", null},
-			{"au.edu.anu.rscs.aot", "omugi", "[0.2.12,)", null},
+			{"au.edu.anu.rscs.aot", "omugi", "[0.2.13,)", null},
 			{"au.edu.anu.rscs.aot", "qgraph", "[0.1.3,)", null},
 			{"au.edu.anu.rscs.aot", "aot", "[0.1.6,)", null},
-			{"fr.cnrs.iees.tw-core", "tw-core", "[0.2.4,)", null},
+			{"fr.cnrs.iees.tw-core", "tw-core", "[0.2.5,)", null},
 			{"org.apache.commons", "commons-text", "1.+", null},
 	};
 	
