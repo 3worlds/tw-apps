d7ec4884dda6804709f6e8529297f98a5fd71add
Jacques Gignoux <jacques.gignoux@upmc.fr>
2019-10-17 15:34:26 +0200

    versioning

--- tw-apps/scripts/fr/cnrs/iees/versioning/VersionSettings.java
+++ tw-apps/scripts/fr/cnrs/iees/versioning/VersionSettings.java
@@ -58,7 +58,7 @@ public class VersionSettings {
 			{"au.edu.anu.rscs.aot", "omugi", "[0.2.14,)", null},
 			{"au.edu.anu.rscs.aot", "qgraph", "[0.1.3,)", null},
 			{"au.edu.anu.rscs.aot", "aot", "[0.1.6,)", null},
-			{"fr.cnrs.iees.tw-core", "tw-core", "[0.3.2,)", null},
+			{"fr.cnrs.iees.tw-core", "tw-core", "[0.3.3,)", null},
 			{"org.apache.commons", "commons-text", "1.+", null},
 	};
 	
