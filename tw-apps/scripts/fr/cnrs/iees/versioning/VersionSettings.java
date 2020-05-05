a57ef67eaa2ac0d920d00efcfa92c2e8d6abe45b
Jacques Gignoux <jacques.gignoux@upmc.fr>
2020-05-05 15:39:20 +0200

    updating dependency  version

--- tw-apps/scripts/fr/cnrs/iees/versioning/VersionSettings.java
+++ tw-apps/scripts/fr/cnrs/iees/versioning/VersionSettings.java
@@ -58,7 +58,7 @@ public class VersionSettings {
 			{"au.edu.anu.rscs.aot", "omugi", "[0.2.16,)", null},
 			{"au.edu.anu.rscs.aot", "qgraph", "[0.1.3,)", null},
 			{"au.edu.anu.rscs.aot", "aot", "[0.1.7,)", null},
-			{"fr.cnrs.iees.tw-core", "tw-core", "[0.3.15,)", null},
+			{"fr.cnrs.iees.tw-core", "tw-core", "[0.3.17,)", null},
 			{"org.apache.commons", "commons-text", "[1,)", null},
 	};
 
