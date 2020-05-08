38914197be9f326c7c23373f0984823bf1c34529
Ian Davies <ian.davies@anu.edu.au>
2020-05-08 11:48:31 +1000

    layout options

--- tw-apps/src/au/edu/anu/twapps/mm/layout/LayoutType.java
+++ tw-apps/src/au/edu/anu/twapps/mm/layout/LayoutType.java
@@ -2,8 +2,13 @@ package au.edu.anu.twapps.mm.layout;
 
 public enum LayoutType {
 	OrderedTree,
+	OrderedTree_,
 	RadialTree1,
+	RadialTree1_,
 	RadialTree2,
+	RadialTree2_,
 	SpringGraph,
+	SpringGraph_,
 	LombardiGraph,
+	LombardiGraph_,
 }
