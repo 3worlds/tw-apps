0030f96ca3089df33c5f00363910bbcc977c13e6
Jacques Gignoux <jacques.gignoux@upmc.fr>
2020-08-31 15:13:29 +0200

    dependency management

--- tw-apps/scripts/fr/cnrs/iees/versioning/VersionSettings.java
+++ tw-apps/scripts/fr/cnrs/iees/versioning/VersionSettings.java
@@ -55,10 +55,10 @@ public class VersionSettings {
 	 */
 	protected static String[][] DEPS = {
 			{"fr.ens.biologie", "generics", "[0.1.17,)", null},
-			{"au.edu.anu.rscs.aot", "omugi", "[0.2.16,)", null},
+			{"au.edu.anu.rscs.aot", "omugi", "[0.3.0,)", null},
 			{"au.edu.anu.rscs.aot", "qgraph", "[0.1.3,)", null},
 			{"au.edu.anu.rscs.aot", "aot", "[0.1.8,)", null},
-			{"fr.cnrs.iees.tw-core", "tw-core", "[0.3.20,)", null},
+			{"fr.cnrs.iees.tw-core", "tw-core", "[0.3.22,)", null},
 			{"org.apache.commons", "commons-text", "[1,)", null},
 	};
 
