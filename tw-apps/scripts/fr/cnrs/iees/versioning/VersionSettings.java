329e64ef355dc1038efcd126042ed91c91e112d1
Ian Davies <ian.davies@anu.edu.au>
2021-02-20 09:42:26 +1100

    dependence consistency

--- tw-apps/scripts/fr/cnrs/iees/versioning/VersionSettings.java
+++ tw-apps/scripts/fr/cnrs/iees/versioning/VersionSettings.java
@@ -54,8 +54,8 @@ public class VersionSettings {
 	 *
 	 */
 	protected static String[][] DEPS = {
-			{"fr.ens.biologie", "generics", "[0.2.2,)", null},
-			{"au.edu.anu.rscs.aot", "omugi", "[0.4.0,)", null},
+			{"fr.ens.biologie", "generics", "[0.2.3,)", null},
+			{"au.edu.anu.rscs.aot", "omugi", "[0.4.1,)", null},
 			{"au.edu.anu.rscs.aot", "qgraph", "[0.1.4,)", null},
 			{"au.edu.anu.rscs.aot", "aot", "[0.1.8,)", null},
 			{"fr.cnrs.iees.tw-core", "tw-core", "[0.3.27,)", null},
