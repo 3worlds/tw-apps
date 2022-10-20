4312f22c93701a2a15b34e736225fdd0e547575d
Jacques Gignoux <jacques.gignoux@upmc.fr>
2022-10-20 13:47:43 +0200

    Package refactoring + code cleanup + version upgrade

--- tw-apps/scripts/fr/cnrs/iees/versioning/VersionSettings.java
+++ tw-apps/scripts/fr/cnrs/iees/versioning/VersionSettings.java
@@ -82,13 +82,13 @@ public class VersionSettings {
 	 *
 	 */
 	protected static String[][] DEPS = {
-			{"fr.cnrs.iees.omhtk", "generics", "[0.4.0,)", null},
-			{"fr.cnrs.iees.omugi", "omugi", "[0.5.0,)", null},
-			{"au.edu.anu.qgraph", "qgraph", "[0.2.0,)", null},
-			{"au.edu.anu.aot", "aot", "[0.2.0,)", null},
-			{"fr.cnrs.iees.tw-core", "tw-core", "[0.6.4,)", null},
+			{"fr.cnrs.iees.omhtk", "generics", "[0.5.0,)", null},
+			{"fr.cnrs.iees.omugi", "omugi", "[0.6.0,)", null},
+			{"au.edu.anu.qgraph", "qgraph", "[0.3.0,)", null},
+			{"au.edu.anu.aot", "aot", "[0.2.1,)", null},
+			{"fr.cnrs.iees.tw-core", "tw-core", "[0.6.5,)", null},
 			{"org.apache.commons", "commons-text", "[1,)", null},
-			{"fr.cnrs.iees.tw-models", "tw-models", "[0.0.5,)", null},
+			{"fr.cnrs.iees.tw-models", "tw-models", "[0.0.6,)", null},
 	};
 
 	/** The name of the main class to put in the jar manifest, if any. This enables users to
